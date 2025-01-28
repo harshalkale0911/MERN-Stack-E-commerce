d83cf559faf172e4050cb606e3756ef5d78187af
harshalkale0911 <hkale6888@gmail.com>
2025-01-28 21:08:01 +0530

    mile 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import {LoginPage,  SignupPage,} from "./Routes.js";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  LoginPage,
+  SignupPage,
+  Home,
+  CreateProduct,
+  MyProducts,
+  Cart,
+  ProductDetails,
+  Profile,
+  CreateAddress,
+} from "./Routes.js";
 import "./App.css";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignupPage />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/create-product" element={<CreateProduct />} />
+        <Route path="/create-product/:id" element={<CreateProduct />} />
+        <Route path="/my-products" element={<MyProducts />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create-address" element={<CreateAddress />} />
+      </Routes>
     </BrowserRouter>
-    
-  )
-}
-
-export default App
+  );
+};
+export default App;
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -3,7 +3,7 @@ import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
 import { RxAvatar } from "react-icons/rx";
-import axios from 'axios';
+import axios from "axios";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -15,47 +15,35 @@ const Signup = () => {
   const handleFileSubmit = (e) => {
     const file = e.target.files[0];
     if (file) {
-      // const allowedTypes = ["image/jpeg", "image/png"];
-      // if (!allowedTypes.includes(file.type)) {
-      //   alert("Invalid file type. Please select a .jpg or .png file.");
-      //   return;
-      // }
-      // if (file.size > 2 * 1024 * 1024) { // 2MB size limit
-      //   alert("File size exceeds 2MB. Please choose a smaller file.");
-      //   return;
-      // }
-
       const filePath = URL.createObjectURL(file);
       console.log("File path:", filePath);
       setAvatar(file);
     }
   };
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const newForm = new FormData();
-    // newForm.append("file", fs.createReadStream(avatar))
     newForm.append("file", avatar);
     newForm.append("name", name);
     newForm.append("email", email);
     newForm.append("password", password);
-    // for (let pair of newForm.entries()) {
-    //   console.log(pair[0] + ': ' + pair[1]);
-    // }
     const config = {
       headers: {
         "Content-Type": "multipart/form-data",
-        "Accept": "any",
+        Accept: "any",
       },
     };
 
-    axios.post("http://10.1.77.124:8000/api/v2/user/create-user", newForm, config).then((res) => {
-      console.log(res.data);
-    }).catch((err) => {
-      console.log(err);
-    });
+    axios
+      .post("http://localhost:8000/api/v2/user/create-user", newForm, config)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
