f8c65c1a51d9f87ba38f98804bb17da9de5e8992
harshalkale0911 <hkale6888@gmail.com>
2025-01-28 22:34:24 +0530

    mile8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,13 +20,13 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
-        <Route path="/create-product" element={<CreateProduct />} />
+        {/* <Route path="/create-product" element={<CreateProduct />} />
         <Route path="/create-product/:id" element={<CreateProduct />} />
         <Route path="/my-products" element={<MyProducts />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/create-address" element={<CreateAddress />} />
+        <Route path="/create-address" element={<CreateAddress />} /> */}
       </Routes>
     </BrowserRouter>
   );
