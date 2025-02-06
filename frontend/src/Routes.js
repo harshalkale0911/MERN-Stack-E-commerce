86016f6e3b91bc01e94a83807d96232b6c2f008f
harshalkale0911 <hkale6888@gmail.com>
2025-02-06 18:12:35 +0530

    mile_11

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,5 @@
-import LoginPage from "../src/pages/Login.jsx";
-import SignupPage from "../src/pages/SignupPage.jsx";
-import Home from "./pages/Home.jsx";
-import CreateProduct from "./pages/createProduct.jsx";
-
-export {
-  SignupPage,
-  LoginPage,
-  Home,
-  CreateProduct,
-};
+import LoginPage from "./pages/Login";
+import SignupPage from "./pages/Signup";
+import Home from "./pages/Home";
+import CreateProduct from './pages/createProduct.jsx';
+export {LoginPage,SignupPage,Home,CreateProduct};
